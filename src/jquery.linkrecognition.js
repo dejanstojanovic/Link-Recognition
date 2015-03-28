3fdf8d43af6bc9d2baf08e49873fea97d1b04102
Dejan Stojanovic <d.stojanovic@hotmail.com>
2015-03-28 16:50:01 +0400

    Added settings

--- src/jquery.linkrecognition.js
+++ src/jquery.linkrecognition.js
@@ -1,11 +1,12 @@
 ﻿(function ($) {
-	$.fn.LinkRecognition = function (options) {
-		var defaults = {
-			cssclass: '',
-			target:'_blank'
-		}
-
-		var settings = $.extend({}, defaults, options);
-		var selector = $(this);
-	}
+    $.fn.LinkRecognition = function (options) {
+        var defaults = {
+            cssClass: '',
+            target: '_blank'
+        }
+        var settings = $.extend({}, defaults, options);
+        var selector = $(this);
+        var regex = /(https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w\/_\.]*(\?\S+)?)?)?)/ig
+        $(selector).html($(selector).html().replace(regex, "<a href='$1' target='" + settings.target + "' class='" + settings.cssClass + "'>$1</a>"));
+    }
 })(jQuery);
